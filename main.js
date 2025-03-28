fe9595df96f924d7b4f950191ea47ffb9ade412b
trignomtry <northruptrig@gmail.com>
2025-03-28 16:53:38 -0400

    Update

--- main.js
+++
@@ -1,7 +0,0 @@
-document.getElementById("solutionsbutton").onclick = function() {
-    window.open("solutions.html", "_self")
-}
-
-document.getElementsByName.onclick = function() {
-    window.open("index.html", "_self")
-}
