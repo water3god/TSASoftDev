44b52d174e0bc0627df5a2ff97aad4141ac21e84
Roger Shi <rogershi123@gmail.com>
2025-03-28 16:51:40 -0400

    commit update

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 document.getElementById("solutionsbutton").onclick = function() {
-    window.open("solutions.html")
+    window.open("solutions.html", "_self")
 }
 
 document.getElementsByName.onclick = function() {
-    window.open("index.html")
+    window.open("index.html", "_self")
 }
