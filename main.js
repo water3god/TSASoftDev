385789c68d65483b46227ba1cfebf51212ffc8d0
Roger Shi <rogershi123@gmail.com>
2025-03-28 15:59:03 -0400

    commit

---
+++ main.js
@@ -0,0 +1,7 @@
+document.getElementById("solutionsbutton").onclick = function() {
+    window.open("solutions.html")
+}
+
+document.getElementsByName.onclick = function() {
+    window.open("index.html")
+}
